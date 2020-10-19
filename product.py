8f7ba8661cb673fb201d65e4b26dafc1f6685c7c
Brad Hsu <1980bradhsu@gmail.com>
2020-10-19 20:04:29 +0800

    add print 2d list code

--- product.py
+++ product.py
@@ -1,13 +1,13 @@
-product = []
+products = []
 while True:
 	name = input('Enter name of the product: ')
 	if name == 'q':
 		break
 	price = input('Enter the price: ')
-	p = []
-	p.append(name)
-	p.append(price)
-	product.append(p)
-print(product)
+	products.append([name, price])
+print(products)
 
-print(product[0][0])
+print(products[0][0])
+
+for p in products:
+	print(p[0], ' is $', p[1])
