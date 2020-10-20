127949721d907f961a5d390f79150c4e6c9e3997
Brad Hsu <1980bradhsu@gmail.com>
2020-10-20 19:05:07 +0800

    add file reading and temove header

--- product.py
+++ product.py
@@ -1,6 +1,15 @@
-
-
 products = []
+
+# read the file
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'Product, Price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+         
+
+
 while True:
 	name = input('Enter name of the product: ')
 	if name == 'q':
@@ -9,12 +18,11 @@ while True:
 	products.append([name, price])
 print(products)
 
-print(products[0][0])
 
 for p in products:
 	print(p[0], ' is $', p[1])
 
 with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('Product, Price')
+	f.write('Product, Price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
