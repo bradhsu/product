028f81d3439cf759f98002978fc309a86a8ec693
Brad Hsu <1980bradhsu@gmail.com>
2020-10-20 18:40:49 +0800

    add header

--- product.py
+++ product.py
@@ -1,3 +1,5 @@
+
+
 products = []
 while True:
 	name = input('Enter name of the product: ')
@@ -11,3 +13,8 @@ print(products[0][0])
 
 for p in products:
 	print(p[0], ' is $', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('Product, Price')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
